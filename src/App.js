559c12c80a83085e758295557af614889de936ff
sagar455 <sagarkr4560@gmail.com>
2023-06-29 03:17:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className="text-center text-3xl font-bold">stylöCart"</h1>
+     <h1 className="text-center text-3xl font-bold">stylöCart</h1>
     </div>
   );
 }
